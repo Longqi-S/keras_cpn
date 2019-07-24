4b6e4ee7175ec5116c18b15b9bfd271b6ee19df4
Nikita Kodenko <kodenkonikita@gmail.com>
2019-07-24 07:03:21 +0200

    Updated models/cpn.py for Keras 2.2.4

--- models/cpn.py
+++ models/cpn.py
@@ -164,7 +164,7 @@ class CPN():
         exlude: list of layer names to excluce
         """
         import h5py
-        from keras.engine import topology
+        from keras.engine import saving
 
         if exclude:
             by_name = True
@@ -186,7 +186,7 @@ class CPN():
             layers = filter(lambda l: l.name not in exclude, layers)
 
         if by_name:
-            topology.load_weights_from_hdf5_group_by_name(f, layers)
+            saving.load_weights_from_hdf5_group_by_name(f, layers)
         else:
             topology.load_weights_from_hdf5_group(f, layers)
         if hasattr(f, 'close'):
